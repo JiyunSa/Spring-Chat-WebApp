e2409b9c88dd9f44e427b65ed5c0060465f69025
JiyunSa <81289580+JiyunSa@users.noreply.github.com>
2021-04-18 10:57:31 +0900

    Add files via upload

---
+++ SP-CW2-demo/src/main/java/com/sp/cw2/demo/SpCw2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.sp.cw2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpCw2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpCw2DemoApplication.class, args);
+	}
+
+}
---
+++ SP-CW2-demo/src/test/java/com/sp/cw2/demo/SpCw2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.cw2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpCw2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
